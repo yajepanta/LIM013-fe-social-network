ac419ecf1f5bf511f0b25c249292ffc55edced1a
wendy-gs <wendy.gonzales.salcedo@gmail.com>
2020-09-25 20:07:13 -0500

    login fb y google

--- src/controller/router.js
+++ src/controller/router.js
@@ -1,31 +1,34 @@
-import {components} from '../views/index.js'
-//Funcion de cambios de rutas
-export const changeView=(hash)=>{
-	const headerElem=document.querySelector("#nav");
-    const container=document.getElementById("container");
-    container.innerHTML="";
-    switch(hash){
-        case "":
-				case "#/Cerrar":{
-					headerElem.classList.remove("mostrar");
-					container.appendChild(components.login());
-					break;
-				}
-				case"#/Registro":{
-					container.appendChild(components.register());
-					break;
-				}
-				case "#/Inicio":{
-					console.log("inicio");
-					headerElem.classList.add("mostrar");
-					container.appendChild(components.timeline());
-					break;
-				}
-				case "#/Perfil":{
-					console.log("perfil");
-					container.appendChild(components.perfil());
-					break;
-				}
-
-        }
+import { components } from '../views/index.js';
+//  Funcion de cambios de rutas
+export const changeView = (hash) => {
+  const headerElem = document.querySelector('#nav');
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  switch (hash) {
+    case '':
+    case '#/Cerrar': {
+      headerElem.classList.remove('mostrar');
+      container.appendChild(components.login());
+      break;
     }
+    case '#/Registro': {
+      container.appendChild(components.register());
+      break;
+    }
+    case '#/Inicio': {
+      console.log('inicio');
+      headerElem.classList.add('mostrar');
+      container.appendChild(components.timeline());
+      break;
+    }
+    case '#/Perfil': {
+      console.log('perfil');
+      container.appendChild(components.perfil());
+      break;
+    }
+    default: {
+      container.innerHTML = 'no encontrado';
+      break;
+    }
+  }
+}
--- src/model/firebase-auth.js
+++ src/model/firebase-auth.js
@@ -1,3 +1,2 @@
-export const logIn =(email,paswword)=>
-    firebase.auth().signInWithEmailAndPassword(email,paswword);
-
+export const logIn = (email, paswword) => firebase.auth()
+  .signInWithEmailAndPassword(email, paswword);
