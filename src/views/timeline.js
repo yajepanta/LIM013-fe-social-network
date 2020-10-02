8138246ef38a706b7786074b88d13c9bfa23b6e2
YajePanta <yajepanta@gmail.com>
2020-10-01 20:29:19 -0500

    estilos de timeline: crear post, posteados, perfil. Desktop y responsive.

--- src/views/timeline.js
+++ src/views/timeline.js
@@ -8,7 +8,7 @@ export const timelineView = () => {
     <!-- PERFIL CON OPCIÓN PARA POSTEAR -->
   <section id="timelineView">
     <section id="profile" class="card">
-      <img src="img/perfil.png" id='photo-profile' class='rounded' alt="profile-picture">
+      <img src="img/perfil.jpg" id='photo-profile' class='rounded' alt="profile-picture">
       <ul class="profile-data">
         <li class="name"><i class="fas fa-id-card"></i> Profesora</li>
         <li class="grade"><i class="fas fa-graduation-cap"></i> 1ro de secundaria</li>
@@ -16,46 +16,70 @@ export const timelineView = () => {
       </ul>
     </section>
 
-    <section class="post card">
-      <div class="post-options">
-        <div id="privacy-post" class="dropdown button-post">
-          <button class="dropbtn">Privacidad</button>
-          <div class="dropdown-content">
-            <a href="#"><i class="fas fa-globe-americas"></i></a>
-            <a href="#"><i class="fas fa-user-lock"></i></a>
+    <section id="create-post">
+      <div class='post card'>
+        <div class="card-header white">
+          <div id="privacy-post" class="dropdown post-btn">
+            <button class="dropbtn"><i class="fas fa-globe-americas"></i> Público <i class="fas fa-caret-down"></i></button>
+            <div class="dropdown-content">
+              <a href="#"><i class="fas fa-globe-americas"></i> Público</a>
+              <a href="#"><i class="fas fa-user-lock"></i> Privado</a>
+            </div>
           </div>
+          <button type="button" id="edit-post" class="post-btn"><i class="fas fa-edit"></i> Editar</button>
+          <button type='button' class='button-delete-post post-btn'><i class="fas fa-trash-alt"></i></button>      
         </div>
-        <a href="#"><i class="fas fa-globe-americas"></i></a>
-        <button type="button" id="edit-post" class="button-post"><i class="fas fa-edit"></i> Editar</button>      
-      </div>
 
-      <div class="text-container">
-        <label for="post-text"></label>
-        <textarea id="post-text" class="post-input-text" name="post-text" placeholder="¿Qué quieres compartir?">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</textarea>
-      </div>
+        <div class="card-text">
+          <label for="post-text"></label>
+          <textarea id="post-text" class="post-input-text" name="post-text" placeholder="¿Qué quieres compartir?">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</textarea>
+        </div>
 
-      <div class="post-buttons">
-        <button type="button" id="load-img" class="button-post"><i class="fas fa-images"></i> Subir imagen</button>
-        <button id="share-post" class="button-post"><i class="fas fa-share-square"></i> Compartir</button>
-      </div>
+        <div class="card-footer">
+          <button type="button" id="load-img" class="post-btn"><i class="fas fa-images"></i> Subir imagen</button>
+          <button id="share-post" class="post-btn"><i class="fas fa-share-square"></i> Compartir</button>
+        </div>
+      </div>  
     </section>
 
     <!-- SECCIÓN CON LOS DEMÁS POSTS -->
-    <section id="timeline" class="posted card">
-      <!-- ocultar íconos de borrar y editar -->
-      <div class="post-author">
-        <img src="img/perfil.png" class='post-profile-picture rounded' alt="profile-picture"><span id='post-author-name'>PROFESOR ABCDEF</span>      
-      </div>
+    <section id="timeline">
+        <!-- ocultar íconos de borrar y editar -->
+      <div class="post card">
+        <div class="card-header">
+          <img src="img/perfil.jpg" class='post-profile-picture rounded' alt="profile-picture">
+          <span id='post-author-name'>PROFESORA ABCDEF</span> 
+             
+        </div>
 
-      <div class="text-container">
-        <label for="post-text-posted"></label>
-        <textarea class="post-input-text" name="post-text-posted" readonly>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</textarea>
-      </div>
+        <div class="card-text">
+          <label for="post-text-posted"></label>
+          <textarea class="post-input-text" name="post-text-posted" readonly>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</textarea>
+        </div>
 
-      <div class="post-buttons">
-        <button type="button" id="like-post" class="button-post"><i class="fas fa-thumbs-up"></i> Me gusta</button>
-        <button id="share-post" class="button-post"><i class="fas fa-comments"></i> Comentar</button>
-      </div>
+        <div class="card-footer">
+          <button type="button" id="like-post" class="post-btn"><i class="fas fa-thumbs-up"></i> Me gusta</button>
+          <button id="share-post" class="post-btn"><i class="fas fa-comments"></i> Comentar</button>
+        </div>
+      </div>  
+
+      <div class="post card">
+        <div class="card-header">
+          <img src="img/perfil.jpg" class='post-profile-picture rounded' alt="profile-picture">
+          <span id='post-author-name'>PROFESORA ABCDEF</span> 
+             
+        </div>
+
+        <div class="card-text">
+          <label for="post-text-posted"></label>
+          <textarea class="post-input-text" name="post-text-posted" readonly>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</textarea>
+        </div>
+
+        <div class="card-footer">
+          <button type="button" id="like-post" class="post-btn"><i class="fas fa-thumbs-up"></i> Me gusta</button>
+          <button id="share-post" class="post-btn"><i class="fas fa-comments"></i> Comentar</button>
+        </div>
+      <div>  
     </section>
   </section> `;
   const div = document.createElement('div');
